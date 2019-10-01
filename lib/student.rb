6d9a0793d557e360aa7c7780b4e4af5c34a7b8a5
Marc Blanchard <marcblanchard0930@gmail.com>
2019-10-01 15:54:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,44 @@
+require 'pry'
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
   
+  attr_accessor :name, :grade
+  attr_reader :id
+  
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id 
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+    #binding.pry
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    sql = <<-SQL 
+      INSERT INTO students (name, grade)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+  
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 end
